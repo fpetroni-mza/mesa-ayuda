a2b0068daf02028ecb2e5e61f99aba35b847c99a
fpetroni-mza <112593981+fpetroni-mza@users.noreply.github.com>
2023-01-31 21:10:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
-<!-- START Botmaker WebChat-->
-<script>
-    (function () {
-        let js = document.createElement('script');
-        js.type = 'text/javascript';
-        js.async = 1;
-        js.src = 'https://go.botmaker.com/rest/webchat/p/FP0YR40T0S/init.js';
-        document.body.appendChild(js);
-    })();
-</script>
-<!-- END Botmaker WebChat-->
+
